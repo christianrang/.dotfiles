9d4c6db42f882e0662dbcb6ba5844f25bca00063
Christian Rang <work@rang-corp.com>
2023-12-05 03:53:13 -0500

    chore: clean up arch installer

--- installers/arch/install.sh
+++ installers/arch/install.sh
@@ -3,12 +3,20 @@
 # Lazy way to install packages. Ideally this will be handled by ansible one day
 
 sudo pacman -Syu
-sudo pacman -S stow
-sudo pacman -S neovim 
-sudo pacman -S go
-sudo pacman -S npm
-sudo pacman -S python
-sudo pacman -S python-pip
+sudo pacman -S stow \
+	alacritty \
+	neovim \
+	go \
+	npm \
+	python \
+	python-pip \
+	tmux
+
+# install asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
+
+# install tmux tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # install vim-plug
 # sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
@@ -23,6 +31,9 @@ go install golang.org/x/tools/gopls@latest
 go install github.com/nametake/golangci-lint-langserver@latest
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.42.1
 
+# install tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
+
 # install pyright
 sudo npm install -g pyright
 
@@ -34,4 +45,3 @@ chmod +x ~/.local/bin/rust-analyzer
 # install rls
 rustup update
 rustup component add rls rust-analysis rust-src
-
