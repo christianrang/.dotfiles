662c226176304260af41db47a35aabf0093d824a
Christian Rang <christian.rang@fishtech.group>
2024-04-09 15:00:20 -0400

    chore(nvim): increasing number of spaces for tabs in config

--- nvim/.config/nvim/lua/personal/set.lua
+++ nvim/.config/nvim/lua/personal/set.lua
@@ -1,10 +1,11 @@
 vim.opt.nu = true
+vim.opt.conceallevel = 0
 
 vim.opt.mouse = nil
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -31,3 +32,4 @@ vim.opt.undofile = true
 vim.cmd('colorscheme catppuccin')
 
 vim.cmd('imap <silent><script><expr> <C-J> copilot#Accept("<CR>")')
+
