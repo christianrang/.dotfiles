26b463a561b295b883ddb4e488621229309a2ae0
Christian Rang <cj@rang-corp.com>
2025-03-16 09:43:07 -0400

    chore(nvim): telescope ignore files

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,5 @@
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fa", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
@@ -6,7 +7,9 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', 'z=', builtin.spell_suggest, {})
 vim.keymap.set('n', '<leader>ht', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>skm', builtin.keymaps, {})
+vim.keymap.set('n', '<leader>sm', builtin.man_pages, {})
 vim.keymap.set('n', '<leader>qf', builtin.quickfix, {})
+vim.keymap.set('n', '<leader>t', builtin.treesitter, {})
 
 require("telescope").load_extension('harpoon')
 require("telescope").load_extension('aerial')
@@ -28,6 +31,16 @@ telescope.setup({
 	defaults = {
 		-- `hidden = true` is not supported in text grep commands.
 		vimgrep_arguments = vimgrep_arguments,
+        file_ignore_patterns = {
+            "node_modules",
+            "vendor",
+            "venv",
+        },
+        mappings = {
+            i = {
+                ["<C-w>"] = "send_selected_to_qflist",
+            }
+        },
 	},
 	pickers = {
 		find_files = {
